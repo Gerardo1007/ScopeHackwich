a0bd035976924c19cb2b39074715c61d77faa06d
GERARDO DEHUMA <rl200066@rlas-macair-23.local>
2018-11-01 10:35:46 -0500

    Initial Commit

---
+++ ScopeHackwich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScopeHackwich
+//
+//  Created by GERARDO DEHUMA on 11/1/18.
+//  Copyright © 2018 GERARDO DEHUMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
